7b14e249453d2fc5f0640236359fbd4b3392ca55
CVD <cornelisvdam@hotmail.com>
2013-12-16 16:10:52 +0100

    HTML data gets saved
    It's very slow :S
    
    To do:
    - add Threading

--- crawler.py
+++ crawler.py
@@ -1,26 +1,34 @@
 import urllib.request
 import string
 import re
+from urllib.error import HTTPError
 
+print ("Vacature crawler v1\n")
+fileName = "data.txt"
+file = open(fileName, "w")
 website = "http://www.starapple.nl/"
 vacature_list = website + "inline/vacatures/"
-pagemax = 99999;
+pagemax = 99999
+currentPos = 1
+startPage = 1
 responses = []
 urls = []
+urlContent = []
 standplaatsen = []
-print ("Loading data...")
+print ("Loading data from " + website)
 
 # browse vacature urls
 
-for page in range(1, pagemax + 1):
+for page in range(startPage, pagemax + 1):
     page_url = vacature_list + str(page)
     print ("Loading " + page_url)
     response = urllib.request.urlopen(page_url)
     html = response.read().decode('utf-8')
     responses.append(html)
-    if(page > 300):
+    if(page > 325):
         foundend = re.findall("Geen vacatures gevonden met de opgegeven zoekcriteria", html);
         if foundend:
+            print (str(page - startPage + 1) + " pages crawled.")
             break
 
 for page in responses:
@@ -28,9 +36,22 @@ for page in responses:
     for url in foundurls:
         urls.append(url)
 
-print (urls)
+print("\nSaving data...")
 
 for url in urls:
-    response = urllib.request.urlopen(website + url);
-    html = response.read().decode('utf-8')
-    # doe iets met de html
+    req = urllib.request.Request(url= website + url,headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'})
+    print("Storing data data from url " + str(currentPos) + "in a list.")
+    currentPos += 1
+    try:
+        response = urllib.request.urlopen(req)
+    except HTTPError as e:
+        content = e.read().decode('utf-8')
+        urlContent.append(content)
+
+urlContentString = ''.join(urlContent)
+print("Saving the HTML data in " + fileName + ".")
+
+file.write(urlContentString)
+file.close()
+print ("\nSave completed.")
+
