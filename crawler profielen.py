8939daf26c8465ffc0f55454f6863fbfa4d07f16
CVD <cornelisvdam@hotmail.com>
2014-01-09 22:47:22 +0100

    Anti starapple commit
    Crawler monsterboard added - doesnt go in for loop very weird
    Crawler.py doesn't work @ starapple anymore
    crawler profieln doesn't work @ starapple

--- crawler profielen.py
+++ crawler profielen.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 import urllib.request
 import string
 import re
@@ -9,35 +8,44 @@ from urllib.error import HTTPError
 
 print ("profile crawler v1\n")
 website = "http://www.starapple.nl/"
-profile_list = website + "kandidaten/#page-"
+profile_list = website + "werkgever/profielen/"
 responses = []
 urls = []
 urlContent = []
 standplaatsen = []
 threadlist = []
 tlist = []
-number_of_threads = 17
+number_of_threads = 4
 
 # browse profile urls
 def removeNonAscii(h): return "".join(i for i in h if ord(i)<128)
 
 def profile_browser(startPage, pagemax):    
     for page in range(startPage, pagemax + 1):
-        page_url = profile_list + str(page)
-        print ("Loading " + page_url)
-        response = urllib.request.urlopen(page_url)
-        html = response.read().decode('utf-8')
-        responses.append(html)
-        if(page > 325):
-            foundend = re.findall(r"TOON MEER RESULTATEN", html);
-            if not foundend:
-                if(page - startPage + 1 != 1):
-                    print (str(page - startPage + 1) + " pages crawled.")
-                break
+        try:
+            page_url = profile_list + str(page) + "/"
+            #print ("Loading " + page_url)
+            response = urllib.request.urlopen(page_url)
+            html = response.read().decode('utf-8')
+            responses.append(html)
+            print("good link")
+            if(page > 325):
+                foundend = re.findall(r"error", html);
+                if foundend:
+                    if(page - startPage + 1 != 1):
+                        print (str(page - startPage + 1) + " pages crawled.")
+                    break
+            
+        except HTTPError as err:
+            if err.code == 404:
+                print("404 ffs")
+                continue
 
-print ("Loading data from " + website + "\n")
+        
+
+print ("Loading profile data from " + website + "\n")
 for i in range(0, number_of_threads):
-    thread = threading.Thread(target = profile_browser, args=(i*10+1, (i+1)*10))
+    thread = threading.Thread(target = profile_browser, args=(i*1*100+1, (i+1)*100))
     thread.setDaemon(1)
     thread.start()
     threadlist.append(thread)
@@ -50,7 +58,7 @@ for page in responses:
     for url in foundurls:
         urls.append(url)
 
-print("\nSaving data...")
+print("\nSaving html data...")
 
 def data_saver(startUrl, endUrl):
     currentUrl = startUrl
@@ -60,7 +68,7 @@ def data_saver(startUrl, endUrl):
             response = urllib.request.urlopen(req)
             html = response.read().decode('utf-8')
             removeNonAscii(html)
-            script_dir = os.path.dirname(__file__) 
+            script_dir = os.path.dirname(__file__)
             rel_path = "raw profile data\index" + str(currentUrl) + ".html"
             abs_file_path = os.path.join(script_dir, rel_path)
             file = open(abs_file_path, "w")
@@ -69,25 +77,30 @@ def data_saver(startUrl, endUrl):
             currentUrl += 1
             
         except HTTPError as e:
-            content = e.read().decode('utf-8')
-            script_dir = os.path.dirname(__file__) 
-            rel_path = "raw profile data\index" + str(currentUrl) + ".html"
-            abs_file_path = os.path.join(script_dir, rel_path)
-            file = open(abs_file_path, "w")
-            file.write(content)
-            file.close()
-            currentUrl += 1
-    
-            
-for i in range(0, number_of_threads):
-    t = threading.Thread(target = data_saver, args=(i*70+1, (i+1)*70))
-    t.setDaemon(1)
-    t.start()
-    tlist.append(t)
+            print("HTTP error terreur hea veelste verward werk nou eens mee starapple")
+            if e.code != 404:
+                print(e.code)
+                content = e.read().decode('utf-8')
+                removeNonAscii(content)
+                script_dir = os.path.dirname(__file__)
+                rel_path = "raw profile data\index" + str(currentUrl) + ".html"
+                abs_file_path = os.path.join(script_dir, rel_path)
+                file = open(abs_file_path, "w")
+                file.write(content)
+                file.close()
+                currentUrl += 1
+                
+            else:
+                print("404 weer wut")
 
+for i in range(0, number_of_threads):
+        t = threading.Thread(target = data_saver, args=(i*1*100+1, (i+1)*100))
+        t.setDaemon(1)
+        t.start()
+        tlist.append(t)
 
 for i in tlist:
     i.join()
-
+    
 print ("\nSave completed.")
 
--- crawler vacatures monsterboard.py
+++ crawler vacatures monsterboard.py
@@ -8,36 +8,43 @@ import os
 from urllib.error import HTTPError
 
 print ("Vacature crawler v1\n")
-website = "http://www.starapple.nl/"
-vacature_list = website + "inline/vacatures/"
+website = "http://jobsearch.monsterboard.nl/"
 responses = []
 urls = []
 urlContent = []
 standplaatsen = []
 threadlist = []
 tlist = []
-number_of_threads = 4
+number_of_threads = 2
 
 # browse vacature urls
 def removeNonAscii(h): return "".join(i for i in h if ord(i)<128)
 
 def vacature_browser(startPage, pagemax):    
     for page in range(startPage, pagemax + 1):
-        page_url = vacature_list + str(page)
-        print ("Loading " + page_url)
+        page_url = website + "browse/?pg=" + str(page) + "&re=nv_gh_gnl1147_%2F"
+        print ("Loading page " + str(page) + "\n")
         response = urllib.request.urlopen(page_url)
         html = response.read().decode('utf-8')
-        responses.append(html)
-        if(page > 325):
-            foundend = re.findall(r"Geen vacatures gevonden met de opgegeven zoekcriteria", html);
-            if foundend:
+        foundend = re.search(r"""box selected" href="http://jobsearch.monsterboard.nl/browse/?pg=1""", html)
+        foundend2 = re.search(r"Pagina niet gevonden", html)
+        if foundend:
+            if(page >= 35):
                 if(page - startPage + 1 != 1):
                     print (str(page - startPage + 1) + " pages crawled.")
                 break
+        if foundend2:
+            if(page >= 35):
+                if(page - startPage + 1 != 1):
+                    print (str(page - startPage + 1) + " pages crawled.")
+                break
+        else:
+            responses.append(html)
 
 print ("Loading data from " + website + "\n")
+
 for i in range(0, number_of_threads):
-    thread = threading.Thread(target = vacature_browser, args=(i*100+1, (i+1)*100))
+    thread = threading.Thread(target = vacature_browser, args=(i*10+1, (i+1)*10))
     thread.setDaemon(1)
     thread.start()
     threadlist.append(thread)
@@ -54,14 +61,17 @@ print("\nSaving data...")
 
 def data_saver(startUrl, endUrl):
     currentUrl = startUrl
+    
     for url in urls[startUrl:endUrl]:
+        print("start for loop")
         req = urllib.request.Request(url= website + url,headers={'User-Agent':' Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'})
         try:
+            print("try")
             response = urllib.request.urlopen(req)
             html = response.read().decode('utf-8')
             removeNonAscii(html)
             script_dir = os.path.dirname(__file__) 
-            rel_path = "raw html data\index" + str(currentUrl) + ".html"
+            rel_path = "raw monsterboard html data\index" + str(currentUrl) + ".html"
             abs_file_path = os.path.join(script_dir, rel_path)
             file = open(abs_file_path, "w")
             file.write(html)
@@ -69,9 +79,10 @@ def data_saver(startUrl, endUrl):
             currentUrl += 1
             
         except HTTPError as e:
+            print("catch")
             content = e.read().decode('utf-8')
             script_dir = os.path.dirname(__file__) 
-            rel_path = "raw html data\index" + str(currentUrl) + ".html"
+            rel_path = "raw monsterboard html data\index" + str(currentUrl) + ".html"
             abs_file_path = os.path.join(script_dir, rel_path)
             file = open(abs_file_path, "w")
             file.write(content)
@@ -80,7 +91,7 @@ def data_saver(startUrl, endUrl):
     
             
 for i in range(0, number_of_threads):
-    t = threading.Thread(target = data_saver, args=(i*700+1, (i+1)*700))
+    t = threading.Thread(target = data_saver, args=(i*25+1, (i+1)*25))
     t.setDaemon(1)
     t.start()
     tlist.append(t)
